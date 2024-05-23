7c6dbf83fa4cd03c185195a1472f3afd0adc40d8
bhavinv2 <114195826+bhavinv2@users.noreply.github.com>
2024-05-23 10:17:08 -0400

    Updated add and view functions book_list.py

--- book_list.py
+++ book_list.py
@@ -1,5 +1,7 @@
 #userinterface milestone 1
 
+import json
+
 def main_menu():
     print("Options:")
     print("1. Add Book")
@@ -10,21 +12,82 @@ def main_menu():
     print("6. Exit")
 
 
+book_list = []
+
+def add_book():
+    title = input ("Enter book title:")
+    author = input("Enter book author:")
+    genre = input("Enter book genre:")
+
+    book_dict= {
+        "Title": title,
+        "Author": author,
+        "Genre": genre,
+    }
+
+    book_list.append(book_dict)
+    # Serializing json
+    json_object = json.dumps(book_list, indent=4)
+
+
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+
+    print(f"Book '{title}' added")
+
+
+
+# def view_book():
+#     try:
+#         with open('sample.json', 'r') as file:
+#             # Reading from json file
+#             json_object = json.load(file)
+#         print(json_object)
+#     except FileNotFoundError:
+#         print("No books")
+
+def view_book():
+
+    with open('sample.json', 'r') as file:
+            # Reading from json file
+        try:
+            json_object = json.load(file)
+        except json.JSONDecodeError:
+            json_object = []
+
+    if not json_object:
+        print("No books available.")
+    else:
+        for index, book in enumerate(json_object, start=1):
+            print(f"{index}. Title: {book['Title']}, Author: {book['Author']}, Genre: {book['Genre']}")
+
+
+
+
+
+
 def main():
     while True:
+
         main_menu()
-        choice = input("Choose an option: ")
-        if choice == "1":
-            #add_book()
-        elif choice == "2":
-            #view_books()
-        elif choice == "3":
-            #rate_book()
-        elif choice == "4":
-            #get_recommendations()
-        elif choice == "5":
-            #search_books()
-        elif choice == "6":
+        user_input = input("Choose an option: ")
+        if user_input == "1":
+            add_book()
+        elif user_input == "2":
+            view_book()
+        elif user_input == "3":
+            print("rate book")
+        elif user_input == "4":
+            print("rec book")
+        elif user_input == "5":
+            print("search book")
+        elif user_input == "6":
             break
         else:
             print("Invalid choice. Please try again.")
+
+
+
+
+
+main()
